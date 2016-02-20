5a5fc7528023933dd52f3f2230b2bf5d5be4ce3c
Artem <ramas93@i.ua>
2016-02-20 16:13:09 +0200

    Added view and template for detailed blog post as well as CSS for the site.

--- bin/firstapp/article/urls.py
+++ bin/firstapp/article/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.post_list, name='post_list'),
+	url(r'^$', views.article_list, name='article_list'),
+	url(r'^article/(?P<pk>[0-9]+)/$', views.article_detail, name='article_detail'),
 ]
--- bin/firstapp/article/views.py
+++ bin/firstapp/article/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Article
+from django.utils import timezone
 
-def post_list(request):
-	return render(request, 'article/post_list.html', {})
+def article_list(request):
+	posts = Article.objects.filter(article_date__lte=timezone.now()).order_by('article_date')
+	return render(request, 'article/post_list.html', {'posts': posts})
+
+def article_detail(request, pk):
+	post = get_object_or_404(Article, pk=pk)
+	return render(request, 'article/post_detail.html', {'post': post})
